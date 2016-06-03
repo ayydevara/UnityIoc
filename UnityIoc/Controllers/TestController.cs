6e9e2d7ef66413dc4694394d15fc1c9a87b07f63
Sreekanth Ayydevara <sreekanth@bedynamic.com>
2016-06-02 20:15:24 -0700

    first commit

---
+++ UnityIoc/Controllers/TestController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Services;
+
+namespace UnityIoc.Controllers
+{
+    [System.Web.Http.RoutePrefix("api/test")]
+    public class TestController : ApiController
+    {
+        private readonly ITestService _testService;
+
+        public TestController(ITestService testService)
+        {
+            _testService = testService;
+        }
+
+        public IEnumerable<string> Get()
+        {
+            return _testService.GetTestValues();
+        }
+
+        [Route("value/{key}")]
+        [HttpGet]
+        public string Value(string key)
+        {
+            return _testService.GetValue(key);
+        }
+
+        [Route("testme")]
+        [HttpGet]
+        public TestMeModel TestMe()
+        {
+            return new TestMeModel
+            {
+                A = "aaaa",
+                B = "xklslskd"
+            };
+        }
+    }
+
+    public class TestMeModel
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+    }
+}
